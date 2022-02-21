402aef63e876e40eca7526f65e8930e7179b6429
David Fachini <david.fachini@gmail.com>
2022-02-21 19:45:00 -0300

    Teste 5

--- tests/dags/test_postgres.py
+++ tests/dags/test_postgres.py
@@ -1,24 +1,40 @@
 # [START import_module]
-from airflow.models import DAG
-from airflow import DAG
+import pandas as pd
+from minio import Minio
+from os import getenv
+from io import BytesIO
+from sqlalchemy import create_engine
 from datetime import datetime, timedelta
-from airflow.operators.bash import BashOperator
+from airflow.models import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.amazon.aws.sensors.s3_key import S3KeySensor
+from airflow.providers.amazon.aws.operators.s3_list import S3ListOperator
+from airflow.providers.amazon.aws.operators.s3_copy_object import S3CopyObjectOperator
+from airflow.providers.amazon.aws.operators.s3_delete_objects import S3DeleteObjectsOperator
 from airflow.operators.postgres_operator import PostgresOperator
 from airflow.hooks.base_hook import BaseHook
-
-
 # [END import_module]
+
+
+# [START env_variables]
+MINIO = getenv("MINIO", "node01.mycirrusit.com:31928")
+ACCESS_KEY = getenv("ACCESS_KEY", "YOURACCESSKEY")
+SECRET_ACCESS = getenv("SECRET_ACCESS", "YOURSECRETKEY")
+POSTGRESQL = getenv("POSTGRESQL", "postgresql://postgres:postgres@k8s.mycirrusit.com:32094/airflow")
+# [END env_variables]
+
 
 # [START Postgres Connector]
 conn = BaseHook.get_connection('postgres')
 print(conn)
 # print(f"AIRFLOW_CONN_{conn.conn_id.upper()}='{conn.get_uri()}'")
-
 # [END Postgres Connector]
 
 # [START default_args]
 default_args = {
     'owner': 'david fachini',
+    'start_date': datetime(2022, 2, 21),
     'depends_on_past': False,
     'email': ['david.fachini@gmail.com'],
     'email_on_failure': False,
@@ -27,32 +43,36 @@ default_args = {
     'retry_delay': timedelta(minutes=5)}
 # [END default_args]
 
-
 # [START instantiate_dag]
-dag_params = {
-    'dag_id': 'PostgresOperator_dag',
-    'start_date': datetime(2022, 2, 21),
-    'schedule_interval': None
-}
-
-with DAG(**dag_params) as dag:
-
-    create_fakerCadastro = PostgresOperator(
-        task_id='create_fakerCadastro',
-        sql='''CREATE TABLE fakerCadastro(
-            custom_id integer NOT NULL, timestamp TIMESTAMP NOT NULL, user_id VARCHAR (50) NOT NULL
-            );''',
-    )
+dag = DAG(
+    'cadastro_faker',
+    default_args=default_args,
+    schedule_interval='@daily',
+    tags=['development', 'faker', 'postgres'])
 # [END instantiate_dag]
 
 # [START basic_task]
+
+# create table on postgres [if not exists] = postgresql [postgres]
+create_fakerCadastro = PostgresOperator(
+    task_id='create_fakerCadastro',
+    postgres_conn_id='postgres_sql',
+    sql='''CREATE TABLE fakerCadastro(
+        custom_id integer NOT NULL, timestamp TIMESTAMP NOT NULL, user_id VARCHAR (50) NOT NULL
+        );''',
+    dag=dag))
+# [END instantiate_dag]
+
+# insert row in postgresql
 insert_faker_row = PostgresOperator(
     task_id='insert_faker_row',
+    postgres_conn_id='postgres_sql',
     sql='INSERT INTO create_fakerCadastro VALUES(%s, %s, %s)',
     # trigger_rule=TriggerRule.ALL_DONE,
     trigger_rule='all_done'
     # parameters=(uuid.uuid4().int % 123456789, datetime.now(), uuid.uuid4().hex[:10])
-)
+    dag=dag)
+
 # [END basic_task]
 
 # [START task_sequence]
