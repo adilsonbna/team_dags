1a462adddc08371128bdb3fc06ac05ef5a0d89f2
Adilson Cesar <adilsonbna@gmail.com>
2022-02-23 10:32:56 -0300

    add new python 3.7

--- tests/dags/twitter_data.py
+++ tests/dags/twitter_data.py
@@ -10,7 +10,7 @@ from airflow.operators.bash import BashOperator
 # These args will get passed on to each operator
 # You can override them on a per-task basis during operator initialization
 default_args = {
-    'owner': 'adilson',
+    'owner': 'Adilson Cesar',
     'depends_on_past': False,
     'email': ['adilsonbna@gmail.com'],
     'email_on_failure': False,
@@ -25,11 +25,11 @@ with DAG(
     schedule_interval=timedelta(minutes=1),
     start_date=datetime(2021, 1, 1),
     catchup=False,
-    tags=['twitter', 'getdata'],
+    tags=['twitter', 'getdata', 'dev'],
 ) as dag:
     t1 = BashOperator(
         task_id='get_tweets',
-        bash_command='/usr/bin/python3.6 /cluster/helm/airflow/Python_twitter.py',
+        bash_command='/usr/bin/python3.7 /cluster/helm/airflow/Python_twitter.py',
     )
 
     t1
