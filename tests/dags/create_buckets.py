68ce94ccc67f71b59b1d72834a8a6e936f1258c6
David Fachini <david.fachini@gmail.com>
2022-02-23 22:51:04 -0300

    Teste Create Buckets

---
+++ tests/dags/create_buckets.py
@@ -0,0 +1,55 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+import pandas as pd
+from faker import Faker
+from minio import Minio
+from minio.error import S3Error
+from airflow.hooks.base_hook import BaseHook
+from airflow.hooks.S3_hook import S3Hook
+
+#conn = BaseHook.get_connection('minio')
+#print(conn)
+
+def gerar_dados_fake():
+    # Criar listas vazias para armazenar os dados gerados para cada coluna do Dataframe
+    # Obs: Ao executar dentro da função irá remover os dados já gerados.
+    # Caso seja necessário manter os dados (append), criar as listas fora da função e comentar esta linha.
+
+
+    TESTE BaseHook
+    conn = BaseHook.get_connection('minio')
+    MINIO = str(conn.host) + ":" + str(conn.port)
+    ACCESS_KEY = str(conn.login)
+    SECRET_ACCESS = str(conn.password)
+    client = Minio(MINIO, ACCESS_KEY, SECRET_ACCESS, secure=False)
+    bucket = "a3team"
+
+# Criar bucket Cadastro e RH
+def cria_buckets()
+    cadastro_faker = cadastro_faker
+    rh_faker = rh_faker
+    bucketC = str(bucket)+"/"+cadastro_faker
+    bucketRH = str(bucket)+"/"+rh_faker 
+    createBucketC = client.bucket_exists(bucketC)
+    createBucketRH = client.bucket_exists(bucketRH)
+    if not createBucketC:
+       client.make_bucket(bucketC)
+    if not bucketRH:
+       client.make_bucket(createBucketRH)
+    return (createBucketC, createBucketRH)
+
+
+
+
+dag = DAG('createBuckets', description='Cria cadastro_faker e rh_faker',
+          schedule_interval=timedelta(minutes=60),
+          start_date=datetime(2017, 3, 20), catchup=False)
+
+create_buckets = PythonOperator(task_id='createBuckets', python_callable=cria_buckets, dag=dag)
+
+
+
+create_buckets
+
